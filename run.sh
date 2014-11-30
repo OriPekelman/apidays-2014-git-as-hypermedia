092773de9c58926591ff827846a03d2bd76ed5cf
Administrator <ori@pekelman.com>
2014-11-30 19:43:39 +0100

    initial commit

---
+++ run.sh
@@ -0,0 +1,1 @@
+pandoc -s index.md -o apidays-2014-git-as-hypermedia.pdf --toc --smart -V documentclass=book -V geometry:margin=1.8in --template=template.latex  -V geometry:paperheight=8.7in  -V geometry:paperwidth=15.399in
